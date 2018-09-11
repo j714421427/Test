1cb4f8260e27e5796007c87cbe2563b851f5b3b9
Sunny <j714421427@hotmail.com>
2018-09-11 16:12:05 +0800

    firstcommit

---
+++ helloworld/helloworld/Models/Info.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace helloworld.Models
+{
+    public class Info
+    {
+        public Info(string  name) {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
